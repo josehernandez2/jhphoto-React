a25746e1f367703b63d4096eea7244ed427f5b50
Jose Hernandez II <jose84hernandez@gmail.com>
2019-11-27 16:04:05 -0600

    Add header component

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {Navbar, NavbarBrand, Nav, NavbarToggler, Collapse,
+  NavItem, Jumbotron} from 'reactstrap';
+import {NavLink} from 'react-router-dom';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isNavOpen: false,
+    };
+    this.toggleNav = this.toggleNav.bind(this);
+  }
+
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark expand="md">
+          <div className="container">
+            <NavbarToggler onClick={this.toggleNav} />
+            <NavbarBrand className="mr-auto" href="/">
+              <img src="assets/images/logo.png" height="41" width="41"
+                alt="Jose Hernandez II photography logo" />
+            </NavbarBrand>
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to="/home">
+                    <span className="fa fa-home fa-lg"></span> Home
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/photos">
+                    <span className="fas fa-camera-retro fa-lg"></span> Photos
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/contact">
+                    <span className="fas fa-envelope fa-lg"></span> Contact
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/about">
+                    <span className="fa fa-id-card fa-lg"></span> About
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
+        </Navbar>
+        <Jumbotron>
+          <div className="container">
+            <div className="row row-header">
+              <div className="col-12 col-sm-6">
+                <h1>Portfolio of Jose Hernandez II</h1>
+                <p>Clean, contemporary photography</p>
+              </div>
+            </div>
+          </div>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+export default Header;
