2572fc0b7af2abcaaf2e4fdcab165cb8acb9a907
Jose Hernandez II <jose84hernandez@gmail.com>
2019-11-27 13:17:41 -0600

    Add Main and Footer Components

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Footer(props) {
+  return(
+    <div className='footer'>
+      <div className='container'>
+        <div className='row'>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
