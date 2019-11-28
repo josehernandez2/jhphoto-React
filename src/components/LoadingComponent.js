b732f8b663ee036f80b8f645c6dd8dc3aa6da5f5
Jose Hernandez II <jose84hernandez@gmail.com>
2019-11-27 18:01:08 -0600

    Add Loading Component

---
+++ src/components/LoadingComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Loading = () => {
+  return (
+    <div className="col-12">
+      <div className="row">
+        <span
+          className="fa fa-sync-alt fa-pulse fa-3x fa-fw text-primary">
+        </span>
+      </div>
+    </div>
+  );
+};
