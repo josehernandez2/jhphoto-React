3b7522c7bcac741ece490b7fff5fc4c39880984c
Jose Hernandez II <jose84hernandez@gmail.com>
2019-11-28 12:34:51 -0600

    Change animated icon to circle o notch

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -5,7 +5,7 @@ export const Loading = () => {
     <div className="col-12">
       <div className="row">
         <span
-          className="fa fa-sync-alt fa-pulse fa-3x fa-fw text-primary">
+          className="fa fa-circle-o-notch fa-spin fa-3x fa-fw text-primary">
         </span>
       </div>
     </div>
