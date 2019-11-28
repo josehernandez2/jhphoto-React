e8a031c0f0598a8339291e7a57eb68bf80191e97
Jose Hernandez II <jose84hernandez@gmail.com>
2019-11-28 11:41:59 -0600

    Import router and animations, Create mapStateToProps and mapDispatchToProps

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
+
 
 class Main extends React.Component {
   render() {
