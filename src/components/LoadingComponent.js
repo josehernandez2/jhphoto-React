c1a815fcd4c1ac25fb7aa1ced3cf53c7f77b525b
Jose Hernandez II <jose84hernandez@gmail.com>
2019-11-29 10:30:52 -0600

    Fix layout of loading component for centered loading icon on photos page

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 export const Loading = () => {
   return (
-    <>
-      <div className="col-4"></div>
-      <div className="col-4 m-5">
+    <div className="col m-5">
+      <div className="row">
         <span
           className="fa fa-circle-o-notch fa-spin fa-3x fa-fw text-primary">
         </span>
       </div>
-      <div className="col-4"></div>
-    </>
+    </div>
   );
 };
