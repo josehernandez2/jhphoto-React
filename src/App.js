60d10f7b8702d9d13b65a8324fa32ba3639f9874
Jose Hernandez II <jose84hernandez@gmail.com>
2019-11-28 19:44:13 -0600

    Add className to Main component for future styles

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
       <Provider store={store}>
         <BrowserRouter>
           <div className="App">
-            <Main />
+            <Main className="main" />
           </div>
         </BrowserRouter>
       </Provider>
