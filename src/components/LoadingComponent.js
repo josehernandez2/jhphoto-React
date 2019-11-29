5ac1576bdf0fd8d6f03a51619ba771fc551cc5e8
Jose Hernandez II <jose84hernandez@gmail.com>
2019-11-28 19:46:42 -0600

    Change layout of Loading component to be centered

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 export const Loading = () => {
   return (
-    <div className="col-12">
-      <div className="row">
+    <>
+      <div className="col-4"></div>
+      <div className="col-4 m-5">
         <span
           className="fa fa-circle-o-notch fa-spin fa-3x fa-fw text-primary">
         </span>
       </div>
-    </div>
+      <div className="col-4"></div>
+    </>
   );
 };
