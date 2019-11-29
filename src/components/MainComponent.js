9ac24ef0dcc1521e3e44fad0a034c6699b4da574
Jose Hernandez II <jose84hernandez@gmail.com>
2019-11-29 10:33:07 -0600

    Add photos component to const Photopage to link to photos

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Photos from './PhotoComponent';
+import {PhotosCardColumn} from './PhotosColumnComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
@@ -48,6 +49,30 @@ class Main extends React.Component {
           carouselErrMess={this.props.items.errMess}/>
       );
     };
+    const PhotoPage = () => {
+      return (
+        <Photos portrait={this.props.portraits.portraits.filter(
+            (portrait) => portrait.featured)[0]}
+        portraitsLoading={this.props.portraits.isLoading}
+        portraitsErrMess={this.props.portraits.errMess}
+        landscape={this.props.landscapes.landscapes.filter(
+            (landscape) => landscape.featured)[0]}
+        landscapesLoading={this.props.landscapes.isLoading}
+        landscapesErrMess={this.props.landscapes.errMess}
+        wildlife={this.props.wildlife.wildlife.filter(
+            (wildlife) => wildlife.featured)[0]}
+        wildlifeLoading={this.props.wildlife.isLoading}
+        wildlifeErrMess={this.props.wildlife.errMess} />
+      );
+    };
+    const PhotosCategory = ({match}) => {
+      return(
+        <PhotosCardColumn
+          portrait={this.props.portraits.portraits}
+          isLoading={this.props.portraits.isLoading}
+          errMess={this.props.portraits.errMess}/>
+      );
+    };
     return (
       <div>
         <Header/>
@@ -56,7 +81,8 @@ class Main extends React.Component {
             timeout={300} classNames="page">
             <Switch>
               <Route path='/home' component={HomePage} />
-              <Route exact path='/photos' component={() => <Photos />} />
+              <Route exact path='/photos' component={PhotoPage} />
+              <Route path="/photos/:category" component={PhotosCategory} />
               <Redirect to='/home' />
             </Switch>
           </CSSTransition>
