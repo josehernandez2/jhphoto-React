904dd96bb8fc5828c40e6f1a99f2535b26fff31d
Jose Hernandez II <jose84hernandez@gmail.com>
2019-11-29 10:35:07 -0600

    Add RenderCard function to render portraits, landscapes and wildlife cards in photos page

--- src/components/PhotoComponent.js
+++ src/components/PhotoComponent.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import {Card, CardHeader, CardBody, CardText,
+  Breadcrumb, BreadcrumbItem, Button} from 'reactstrap';
+import {Loading} from './LoadingComponent';
+import {Link} from 'react-router-dom';
+import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform} from 'react-animation-components';
 
-function Photos() {
+function RenderCard({item, cardIsLoading, errMess}) {
+  if (cardIsLoading) {
+    return (
+      <Loading />
+    );
+  } else if (errMess) {
+    return (
+      <h4>{errMess}</h4>
+    );
+  } else {
+    return (
+      <div className="col-12 col-md m-1">
+        <FadeTransform in
+          transformProps={{exitTransform: 'scale(0.5) translateY(-50%)'}}>
+          <Card>
+            <CardHeader
+              className="text-center" tag="h3">{item.category}
+            </CardHeader>
+            <img width="100%"
+              src={baseUrl + item.image} alt={item.description} />
+            <CardBody className="text-center">
+              <CardText>{item.quote}</CardText>
+              <Link to={'/photos/${item.category}'}>
+                <Button>See More</Button>
+              </Link>
+            </CardBody>
+          </Card>
+        </FadeTransform>
+      </div>
+    );
+  }
+}
+
+function Photos(props) {
   return (
-    <div>
-      Hello World
+    <div className="container">
+      <div className="row mb-5">
+        <Breadcrumb>
+          <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+          <BreadcrumbItem active>Photos</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="row mt-5">
+          <RenderCard item={props.portrait}
+            cardIsLoading={props.portraitsLoading}
+            errMess={props.portraitsErrMess} />
+          <RenderCard item={props.landscape}
+            cardIsLoading={props.landscapesLoading}
+            errMess={props.landscapesErrMess} />
+          <RenderCard item={props.wildlife}
+            cardIsLoading={props.wildlifeLoading}
+            errMess={props.wildlifeErrMess} />
+        </div>
+      </div>
     </div>
   );
 };
