a24695726782c5379f81e4deab18d0a92ad282b9
Jose Hernandez II <jose84hernandez@gmail.com>
2019-11-29 19:20:07 -0600

    Create RenderPhotos component

---
+++ src/components/RenderPhotosComponent.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {Collapse} from 'reactstrap';
+import {Loading} from './LoadingComponent';
+import {Fade, Stagger} from 'react-animation-components';
+import {baseUrl} from '../shared/baseUrl';
+
+const RenderPhotos = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  if (props.isLoading) {
+    return (
+      <Loading />
+    );
+  } else if (props.errMess) {
+    return (
+      <h4>{props.errMess}</h4>
+    );
+  } else {
+    return (
+      <Stagger delay={500} in>
+        {props.photos.map((photos) => {
+          return (
+            <Fade key={photos.id} in>
+              <img
+                className="img-fluid"
+                src={baseUrl + photos.image}
+                alt={photos.name}
+                onClick={toggle} />
+              <Collapse isOpen={isOpen}>
+                <h2>{photos.name}</h2>
+                <p>{photos.description}</p>
+              </Collapse>
+            </Fade>
+          );
+        })}
+      </Stagger>
+    );
+  }
+};
+
+export default RenderPhotos;
