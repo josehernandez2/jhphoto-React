5ef96cb096e4344bd472f17d2e1cfc90f6ca4591
Jose Hernandez II <jose84hernandez@gmail.com>
2019-11-29 19:18:23 -0600

    Import portraits, landscapes, wildlife, and contact components; add PostMessage dispatch; add routes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,13 +1,19 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Photos from './PhotoComponent';
-import {PhotosCardColumn} from './PhotosColumnComponent';
+import Portraits from './PortraitsComponent';
+import Landscapes from './LandscapesComponent';
+import Wildlife from './WildlifeComponent';
+import Contact from './ContactComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
+import {actions} from 'react-redux-form';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
-import {fetchItems, fetchLandscapes, fetchPortraits, fetchWildlife} from '../redux/ActionCreators';
+import {fetchItems, fetchLandscapes,
+  fetchPortraits, fetchWildlife, postMessage} from '../redux/ActionCreators';
 
 const mapStateToProps = (state) => {
   return {
@@ -31,6 +37,26 @@ const mapDispatchToProps = (dispatch) => ({
   fetchWildlife: () => {
     dispatch(fetchWildlife());
   },
+  postMessage: (
+      firstname,
+      lastname,
+      telnum,
+      email,
+      agree,
+      contactType,
+      message) => {
+    dispatch(postMessage(
+        firstname,
+        lastname,
+        telnum,
+        email,
+        agree,
+        contactType,
+        message));
+  },
+  resetMessageForm: () => {
+    dispatch(actions.reset('message'));
+  },
 });
 
 class Main extends React.Component {
@@ -44,33 +70,27 @@ class Main extends React.Component {
   render() {
     const HomePage = () => {
       return (
-        <Home items={this.props.items.items}
+        <Home
+          items={this.props.items.items}
           carouselLoading={this.props.items.isLoading}
           carouselErrMess={this.props.items.errMess}/>
       );
     };
     const PhotoPage = () => {
       return (
-        <Photos portrait={this.props.portraits.portraits.filter(
-            (portrait) => portrait.featured)[0]}
-        portraitsLoading={this.props.portraits.isLoading}
-        portraitsErrMess={this.props.portraits.errMess}
-        landscape={this.props.landscapes.landscapes.filter(
-            (landscape) => landscape.featured)[0]}
-        landscapesLoading={this.props.landscapes.isLoading}
-        landscapesErrMess={this.props.landscapes.errMess}
-        wildlife={this.props.wildlife.wildlife.filter(
-            (wildlife) => wildlife.featured)[0]}
-        wildlifeLoading={this.props.wildlife.isLoading}
-        wildlifeErrMess={this.props.wildlife.errMess} />
-      );
-    };
-    const PhotosCategory = ({match}) => {
-      return(
-        <PhotosCardColumn
-          portrait={this.props.portraits.portraits}
-          isLoading={this.props.portraits.isLoading}
-          errMess={this.props.portraits.errMess}/>
+        <Photos
+          portraitFeatured={this.props.portraits.portraits.filter(
+              (portraits) => portraits.featured)[0]}
+          portraitsLoading={this.props.portraits.isLoading}
+          portraitsErrMess={this.props.portraits.errMess}
+          landscapeFeatured={this.props.landscapes.landscapes.filter(
+              (landscape) => landscape.featured)[0]}
+          landscapesLoading={this.props.landscapes.isLoading}
+          landscapesErrMess={this.props.landscapes.errMess}
+          wildlifeFeatured={this.props.wildlife.wildlife.filter(
+              (wildlife) => wildlife.featured)[0]}
+          wildlifeLoading={this.props.wildlife.isLoading}
+          wildlifeErrMess={this.props.wildlife.errMess} />
       );
     };
     return (
@@ -78,11 +98,29 @@ class Main extends React.Component {
         <Header/>
         <TransitionGroup>
           <CSSTransition key={this.props.location.key}
-            timeout={300} classNames="page">
+            timeout={500} classNames="page">
             <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/photos' component={PhotoPage} />
-              <Route path="/photos/:category" component={PhotosCategory} />
+              <Route path="/photos/portraits" component={() =>
+                <Portraits
+                  portraits={this.props.portraits.portraits}
+                  portraitsLoading={this.props.portraits.isLoading}
+                  portraitsErrMess={this.props.portraits.errMess}/>} />
+              <Route path="/photos/landscapes" component={() =>
+                <Landscapes
+                  landscapes={this.props.landscapes.landscapes}
+                  landscapesLoading={this.props.landscapes.isLoading}
+                  landscapesErrMess={this.props.landscapes.errMess}/>} />
+              <Route path="/photos/wildlife" component={() =>
+                <Wildlife
+                  wildlife={this.props.wildlife.wildlife}
+                  wildlifeLoading={this.props.wildlife.isLoading}
+                  wildlifeErrMess={this.props.wildlife.errMess}/>} />
+              <Route exact path="/contact" component={() =>
+                <Contact
+                  postMessage={this.props.postMessage}
+                  resetMessageForm={this.props.resetMessageForm} />} />
               <Redirect to='/home' />
             </Switch>
           </CSSTransition>
